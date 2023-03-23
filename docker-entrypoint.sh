d0c4974137f9d28509995c921a618028eaae350d
ds.hiveon <x1unix@hiveon.net>
2023-03-23 23:32:12 +0100

    fix: export CXX

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 [ -z "$GOARCH" ] && export GOARCH=$(go env GOARCH);
+[ -z "$GOOS" ] && export GOOS="windows";
 
 # Set toolchain according to GOARCH variable
 case "$GOARCH" in
     amd64)
         export CXX_FOR_TARGET=x86_64-w64-mingw32-g++
         export CC_FOR_TARGET=x86_64-w64-mingw32-gcc
+        export CXX=x86_64-w64-mingw32-g++
         export CC=x86_64-w64-mingw32-gcc
         ;;
     386)
         export CXX_FOR_TARGET=i686-w64-mingw32-g++
         export CC_FOR_TARGET=i686-w64-mingw32-gcc
+        export CXX=i686-w64-mingw32-g++
         export CC=i686-w64-mingw32-gcc
         ;;
     *)
