adfcb8fdcfd7781d41862ff7e6f920755c8efa12
x1unix <denis0051@gmail.com>
2023-03-24 01:01:11 +0100

    chore: refactor pipeline

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
-[ -z "$GOARCH" ] && export GOARCH=$(go env GOARCH);
-[ -z "$GOOS" ] && export GOOS="windows";
+[ -z "$GOARCH" ] && export GOARCH="amd64"
+[ -z "$GOOS" ] && export GOOS="windows"
 
 # Set toolchain according to GOARCH variable
 case "$GOARCH" in
-    amd64)
-        export CXX_FOR_TARGET=x86_64-w64-mingw32-g++
-        export CC_FOR_TARGET=x86_64-w64-mingw32-gcc
-        export CXX=x86_64-w64-mingw32-g++
-        export CC=x86_64-w64-mingw32-gcc
-        ;;
-    386)
-        export CXX_FOR_TARGET=i686-w64-mingw32-g++
-        export CC_FOR_TARGET=i686-w64-mingw32-gcc
-        export CXX=i686-w64-mingw32-g++
-        export CC=i686-w64-mingw32-gcc
-        ;;
-    *)
-        echo "Unsupported GOARCH variable value '$GOARCH'. Please set GOARCH environment variable to 'amd64' or '386'"
-        exit 2
+amd64)
+    export CXX_FOR_TARGET=x86_64-w64-mingw32-g++
+    export CC_FOR_TARGET=x86_64-w64-mingw32-gcc
+    export CXX=x86_64-w64-mingw32-g++
+    export CC=x86_64-w64-mingw32-gcc
+    ;;
+386)
+    export CXX_FOR_TARGET=i686-w64-mingw32-g++
+    export CC_FOR_TARGET=i686-w64-mingw32-gcc
+    export CXX=i686-w64-mingw32-g++
+    export CC=i686-w64-mingw32-gcc
+    ;;
+*)
+    echo "Unsupported GOARCH variable value '$GOARCH'. Please set GOARCH environment variable to 'amd64' or '386'"
+    exit 2
     ;;
 esac
 
